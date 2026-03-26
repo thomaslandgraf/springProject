3733c5f6995af41039469f503ba294abde328590
Thomas Landgraf <thomas.ml@hotmail.com>
2026-03-25 17:36:05 -0700

    OrdemItem

--- src/main/java/com/springproject/firstspring/config/TestConfig.java
+++ src/main/java/com/springproject/firstspring/config/TestConfig.java
@@ -1,14 +1,8 @@
 package com.springproject.firstspring.config;
 
-import com.springproject.firstspring.entities.Category;
-import com.springproject.firstspring.entities.Order;
-import com.springproject.firstspring.entities.Product;
-import com.springproject.firstspring.entities.User;
+import com.springproject.firstspring.entities.*;
 import com.springproject.firstspring.entities.enums.OrderStatus;
-import com.springproject.firstspring.repositories.CategoryRepository;
-import com.springproject.firstspring.repositories.OrderRepository;
-import com.springproject.firstspring.repositories.ProductRepository;
-import com.springproject.firstspring.repositories.UserRepository;
+import com.springproject.firstspring.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
@@ -32,6 +26,9 @@ public class TestConfig implements CommandLineRunner {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private OrderItemRepository orderItemRepository;
 
     @Override
     public void run(String... args) throws Exception {
@@ -67,5 +64,11 @@ public class TestConfig implements CommandLineRunner {
 
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+        OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+        OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+        OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+        orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
     }
 }
--- src/main/java/com/springproject/firstspring/entities/Order.java
+++ src/main/java/com/springproject/firstspring/entities/Order.java
@@ -7,7 +7,9 @@ import org.aspectj.weaver.ast.Or;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 @Entity
 @Table(name = "tb_order")
@@ -27,6 +29,9 @@ public class Order implements Serializable {
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
+
+    @OneToMany(mappedBy ="id.order")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Order(){
     }
@@ -69,6 +74,10 @@ public class Order implements Serializable {
 
     public void setClient(User client) {
         this.client = client;
+    }
+
+    public Set<OrderItem> getItem(){
+        return items;
     }
 
     @Override
