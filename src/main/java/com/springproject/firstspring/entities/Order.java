d42ef6e6e159023936713dab88205d28e6c1c312
Thomas Landgraf <thomas.ml@hotmail.com>
2026-03-25 21:24:58 -0700

    Subtotal & Total methods

--- src/main/java/com/springproject/firstspring/entities/Order.java
+++ src/main/java/com/springproject/firstspring/entities/Order.java
@@ -91,6 +91,14 @@ public class Order implements Serializable {
         this.payment = payment;
     }
 
+    public Double getTotal(){
+        double sum = 0;
+        for(OrderItem x : items){
+            sum += x.getSubTotal();
+        }
+        return sum;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Order order)) return false;
