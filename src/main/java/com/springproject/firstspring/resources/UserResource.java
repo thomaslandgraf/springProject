f344fb547826252ae87d80f707c0183f99f5ae80
Thomas Landgraf <thomas.ml@hotmail.com>
2026-03-26 21:18:17 -0700

    User Delete

--- src/main/java/com/springproject/firstspring/resources/UserResource.java
+++ src/main/java/com/springproject/firstspring/resources/UserResource.java
@@ -36,4 +36,10 @@ public class UserResource {
                 .buildAndExpand(obj.getId()).toUri();
         return ResponseEntity.created(uri).body(obj);
     }
+    
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id){
+        userService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
 }
